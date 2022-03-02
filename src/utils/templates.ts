107f7e50f10586f388fbf2a2eb648ebb57851407
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-03-02 20:32:24 +0100

    Initialize templates

--- src/utils/templates.ts
+++ src/utils/templates.ts
@@ -1,3 +1,23 @@
 import pug from "pug";
+import { TranslateFunction } from "@grammyjs/fluent";
 
-export const wordListTemplate = pug.compileFile("../templates/word-list.pug");
+type DefaultTemplateParameter = Parameters<
+  ReturnType<typeof pug.compileFile>
+>[0];
+
+function compileFile<Props extends DefaultTemplateParameter>(
+  ...args: Parameters<typeof pug.compileFile>
+) {
+  return pug.compileFile(...args) as (
+    locals: Props & {
+      t: TranslateFunction;
+    }
+  ) => string;
+}
+
+interface WordListProps {
+  words: Array<string>;
+}
+export const wordListTemplate = compileFile<WordListProps>(
+  "../templates/word-list.pug"
+);
