3008e7a6c9489226949f951088f2279759e9655a
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-04-05 02:12:43 +0200

    Move constant out of middleware function

--- src/middleware/templates.ts
+++ src/middleware/templates.ts
@@ -2,11 +2,12 @@ import * as templates from "../utils/templates";
 import { NextFunction } from "grammy";
 import { MyContext } from "../context";
 
+const templateKeys = Object.keys(templates) as (keyof typeof templates)[];
+
 export async function useTemplates(
   ctx: MyContext,
   next: NextFunction // is an alias for: () => Promise<void>
 ): Promise<void> {
-  const templateKeys = Object.keys(templates) as (keyof typeof templates)[];
   const templateEntries = templateKeys.map((templateKey) => [
     templateKey,
     (object: any) =>
