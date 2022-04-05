8cfd020ff2f169cb4a98b103afb8ea3f22ea8cdc
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-04-05 02:12:21 +0200

    Handle creating ensemble in start menu

--- src/menus/startMenu.ts
+++ src/menus/startMenu.ts
@@ -1,6 +1,7 @@
 import { Menu } from "@grammyjs/menu";
 import { MyContext } from "../context";
+import { createEnsembleHandler } from "../utils/handlers";
 
 export const startMenu = new Menu<MyContext>("start")
   .text((ctx) => ctx.t("start_join"))
-  .text((ctx) => ctx.t("start_create"));
+  .text((ctx) => ctx.t("start_create"), createEnsembleHandler);
