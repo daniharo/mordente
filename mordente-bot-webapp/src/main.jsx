2e021fe176db4fc86c550c10f656db7384a05dfd
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-04-19 00:34:55 +0200

    Add webapp interface to the bot

---
+++ mordente-bot-webapp/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
