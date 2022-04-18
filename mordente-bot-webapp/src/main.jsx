97776cd5b8bd7372e62c1af29b09c153b7f07a4d
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-04-19 01:13:36 +0200

    Add name input to the webapp interface

--- mordente-bot-webapp/src/main.jsx
+++ mordente-bot-webapp/src/main.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ mordente-bot-webapp/src/routes/create_ensemble.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import WebApp from "@grammyjs/web-app/web-app";
+import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+
+export default function CreateEnsemble() {
+  const [name, setName] = useState("");
+  useEffect(() => {
+    WebApp.MainButton.setParams({ text: "Crear agrupación", is_visible: true });
+    WebApp.MainButton.onClick(() => {
+      WebApp.sendData(JSON.stringify({ name }));
+    });
+  }, []);
+
+  const handleNameChange = (event) => setName(event.target.value);
+
+  return (
+    <FormControl>
+      <FormLabel htmlFor="title">Nombre del ensemble</FormLabel>
+      <Input id="title" type="text" onChange={handleNameChange} value={name} />
+    </FormControl>
+  );
+}
