c90b7b12746d4a8c7759e77b35543a13bde221ed
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-07-15 01:08:51 +0200

    Simplify command analysis

--- src/utils/commandHandler.ts
+++ src/utils/commandHandler.ts
@@ -1,36 +1,18 @@
 import { Message } from "@grammyjs/types";
 
-export enum CommandType {
-  Event,
-  User,
-  Song,
-  Part,
-}
-
-interface ICommandMap {
-  [key: string]: CommandType | undefined;
-}
-
-const CommandMap: ICommandMap = {
-  user: CommandType.User,
-  event: CommandType.Event,
-  song: CommandType.Song,
-  part: CommandType.Part,
-};
-
 interface MordenteCommand {
-  type: CommandType;
+  type: string;
   id: number;
 }
 
 export const analizeCommand = (command: string): MordenteCommand | null => {
   const commandTokens = command.split("_");
-  const prefix = commandTokens.at(0);
-  const type = prefix && CommandMap[prefix];
-  if (!prefix || !type) return null;
+  const type = commandTokens.at(0);
+  const id = commandTokens.at(1);
+  if (!type || !id) return null;
   return {
     type,
-    id: +commandTokens.at(1)!,
+    id: +id,
   };
 };
 
