d0a58c0dabde031b2d68f76d55435f0a4b375da3
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-07-15 02:07:15 +0200

    Check ensemble permissions

--- src/handlers/ensemble.ts
+++ src/handlers/ensemble.ts
@@ -3,6 +3,8 @@ import { MyContext } from "../context";
 import { Ensemble } from "@prisma/client";
 import { ensembleMenu } from "../menus/ensembleMenu";
 import { deleteEnsemble } from "../utils/models/ensemble";
+import { telegramUserIsMember } from "../utils/models/membership";
+import { telegramUserIsAdmin } from "../utils/models/admin";
 
 export const createEnsembleHandler: MiddlewareFn<MyContext> = async (ctx) => {
   await ctx.reply(ctx.t("create_command_answer"), {
@@ -14,6 +16,15 @@ export const createEnsembleHandler: MiddlewareFn<MyContext> = async (ctx) => {
 export const printEnsembleHandler: (
   ensemble: Ensemble
 ) => MiddlewareFn<MyContext> = (ensemble) => async (ctx) => {
+  if (
+    !(await telegramUserIsMember({
+      telegramUserId: ctx.from?.id,
+      ensembleId: ensemble.id,
+    }))
+  ) {
+    ctx.reply("No eres miembro de esta agrupación.");
+    return;
+  }
   ctx.session.ensembleId = ensemble.id;
   await ctx.reply(ctx.templates.ensembleDetailTemplate({ ensemble }), {
     parse_mode: "HTML",
@@ -24,6 +35,15 @@ export const printEnsembleHandler: (
 export const deleteEnsembleHandler: (
   ensemble: Ensemble
 ) => MiddlewareFn<MyContext> = (ensemble) => async (ctx) => {
+  if (
+    !(await telegramUserIsAdmin({
+      telegramUserId: ctx.from?.id,
+      ensembleId: ensemble.id,
+    }))
+  ) {
+    ctx.reply("No eres administrador de esta agrupación.");
+    return;
+  }
   await deleteEnsemble({ ensembleId: ensemble.id });
   await ctx.reply(`La agrupación "${ensemble.name}" ha sido eliminada.`);
 };
--- src/utils/models/membership.ts
+++ src/utils/models/membership.ts
@@ -1,5 +1,6 @@
 import { Ensemble, User } from "@prisma/client";
 import prisma from "../../prisma/PrismaClient";
+import { getUserIdFromUID } from "./user";
 
 export const joinEnsemble = ({
   userId,
@@ -14,4 +15,34 @@ export const joinEnsemble = ({
       ensembleId,
     },
   });
+};
+
+export const telegramUserIsMember = async ({
+  telegramUserId,
+  ensembleId,
+}: {
+  telegramUserId?: User["uid"];
+  ensembleId: Ensemble["id"];
+}) => {
+  if (!telegramUserId) return false;
+  const userId = await getUserIdFromUID({ userUid: telegramUserId });
+  return userId ? userIsMember({ userId, ensembleId }) : false;
+};
+
+export const userIsMember = async ({
+  userId,
+  ensembleId,
+}: {
+  userId: User["id"];
+  ensembleId: Ensemble["id"];
+}) => {
+  const membership = await prisma.membership.findUnique({
+    where: {
+      userId_ensembleId: {
+        userId,
+        ensembleId,
+      },
+    },
+  });
+  return !!membership;
 };
