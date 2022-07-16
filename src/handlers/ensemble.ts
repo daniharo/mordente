d59c71386b9a84fd3b6f26345bfaf378bdda8904
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-07-16 15:23:30 +0200

    Remove use of MiddlewareFn type in handlers

--- src/handlers/ensemble.ts
+++ src/handlers/ensemble.ts
@@ -1,4 +1,3 @@
-import { MiddlewareFn } from "grammy";
 import { MyContext } from "../context";
 import { Ensemble } from "@prisma/client";
 import { ensembleMenu } from "../menus/ensembleMenu";
@@ -6,44 +5,42 @@ import { deleteEnsemble } from "../utils/models/ensemble";
 import { userIsMember } from "../utils/models/membership";
 import { isAdmin } from "../utils/models/admin";
 
-export const createEnsembleHandler: MiddlewareFn<MyContext> = async (ctx) => {
+export const createEnsembleHandler = async (ctx: MyContext) => {
   await ctx.reply(ctx.t("create_command_answer"), {
     reply_markup: { force_reply: true },
   });
   ctx.session.step = "create_ensemble_name";
 };
 
-export const printEnsembleHandler: (
-  ensemble: Ensemble
-) => MiddlewareFn<MyContext> = (ensemble) => async (ctx) => {
-  if (
-    !(await userIsMember({
-      userId: ctx.userId,
-      ensembleId: ensemble.id,
-    }))
-  ) {
-    await ctx.reply("No eres miembro de esta agrupación.");
-    return;
-  }
-  ctx.session.ensembleId = ensemble.id;
-  await ctx.reply(ctx.templates.ensembleDetailTemplate({ ensemble }), {
-    parse_mode: "HTML",
-    reply_markup: ensembleMenu,
-  });
-};
+export const printEnsembleHandler =
+  (ensemble: Ensemble) => async (ctx: MyContext) => {
+    if (
+      !(await userIsMember({
+        userId: ctx.userId,
+        ensembleId: ensemble.id,
+      }))
+    ) {
+      await ctx.reply("No eres miembro de esta agrupación.");
+      return;
+    }
+    ctx.session.ensembleId = ensemble.id;
+    await ctx.reply(ctx.templates.ensembleDetailTemplate({ ensemble }), {
+      parse_mode: "HTML",
+      reply_markup: ensembleMenu,
+    });
+  };
 
-export const deleteEnsembleHandler: (
-  ensemble: Ensemble
-) => MiddlewareFn<MyContext> = (ensemble) => async (ctx) => {
-  if (
-    !(await isAdmin({
-      userId: ctx.userId,
-      ensembleId: ensemble.id,
-    }))
-  ) {
-    await ctx.reply("No eres administrador de esta agrupación.");
-    return;
-  }
-  await deleteEnsemble({ ensembleId: ensemble.id });
-  await ctx.reply(`La agrupación "${ensemble.name}" ha sido eliminada.`);
-};
+export const deleteEnsembleHandler =
+  (ensemble: Ensemble) => async (ctx: MyContext) => {
+    if (
+      !(await isAdmin({
+        userId: ctx.userId,
+        ensembleId: ensemble.id,
+      }))
+    ) {
+      await ctx.reply("No eres administrador de esta agrupación.");
+      return;
+    }
+    await deleteEnsemble({ ensembleId: ensemble.id });
+    await ctx.reply(`La agrupación "${ensemble.name}" ha sido eliminada.`);
+  };
