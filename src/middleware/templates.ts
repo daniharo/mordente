6fd57ee86a89e6367412a0387653f8e302423506
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-07-17 17:56:32 +0200

    Use MiddlewareFn type for templates middleware

--- src/middleware/templates.ts
+++ src/middleware/templates.ts
@@ -1,13 +1,13 @@
 import * as templates from "../utils/templates";
-import { NextFunction } from "grammy";
+import { MiddlewareFn } from "grammy";
 import { MyContext } from "../context";
 
 const templateKeys = Object.keys(templates) as (keyof typeof templates)[];
 
-export async function useTemplates(
-  ctx: MyContext,
-  next: NextFunction // is an alias for: () => Promise<void>
-): Promise<void> {
+export const useTemplates: MiddlewareFn<MyContext> = async (
+  ctx,
+  next // is an alias for: () => Promise<void>
+) => {
   const templateEntries = templateKeys.map((templateKey) => [
     templateKey,
     (object: any) =>
@@ -15,4 +15,4 @@ export async function useTemplates(
   ]);
   ctx.templates = Object.fromEntries(templateEntries);
   await next();
-}
+};
