fae60d3ad35a89e95f92c3243c89aad10a0c46ba
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-07-19 19:18:46 +0200

    Make Calendar a Middleware object with transformer function

--- src/calendar/calendarMenuMiddleware.ts
+++ src/calendar/calendarMenuMiddleware.ts
@@ -1,48 +1,90 @@
-import { CallbackQueryMiddleware, Composer } from "grammy";
-import { CalendarHelper } from "./CalendarHelper";
-import { MyContext } from "../context";
+import { Composer, Context, MiddlewareFn, MiddlewareObj } from "grammy";
+import { CalendarHelper, CalendarOptions } from "./CalendarHelper";
+import { InlineKeyboardMarkup } from "@grammyjs/types";
 
-export const useCalendar = new Composer<MyContext>();
+export class Calendar<T extends CustomContext = CustomContext>
+  implements InlineKeyboardMarkup, MiddlewareObj<T>
+{
+  private readonly extractOptions: (ctx: T) => Partial<CalendarOptions>;
 
-const prevNextMiddleware: (
-  which: "prev" | "next"
-) => CallbackQueryMiddleware<MyContext> = (which) => async (ctx) => {
-  if (!ctx.match) return;
-  const dateString = ctx.match[1];
-  const date = new Date(dateString);
-  date.setMonth(which === "next" ? date.getMonth() + 1 : date.getMonth() - 1);
-  await ctx.answerCallbackQuery();
-  await ctx.editMessageReplyMarkup({
-    reply_markup: new CalendarHelper({}).getCalendarMarkup(date),
-  });
-};
-
-useCalendar.callbackQuery(
-  /calendar-telegram-date-([\d-]+)/,
-  async (ctx, next) => {
-    if (!ctx.match) return;
-    const dateString = ctx.match[1];
-    ctx.calendarSelectedDate = new Date(dateString);
-    await ctx.answerCallbackQuery();
-    await ctx.editMessageReplyMarkup();
-    await next();
+  constructor(extractOptions: (ctx: T) => Partial<CalendarOptions>) {
+    this.extractOptions = extractOptions;
   }
-);
 
-useCalendar.callbackQuery(
-  /calendar-telegram-next-([\d-]+)/,
-  prevNextMiddleware("next")
-);
+  public readonly inline_keyboard = new Proxy([], {
+    get: () => {
+      throw new Error(
+        "Cannot send Calendar menu! Did you forget to use bot.use() for it?"
+      );
+    },
+  });
 
-useCalendar.callbackQuery(
-  /calendar-telegram-prev-([\d-]+)/,
-  prevNextMiddleware("prev")
-);
+  private prepare(payload: Record<string, unknown>, ctx: T) {
+    if (payload.reply_markup instanceof Calendar) {
+      const options = this.extractOptions(ctx);
+      payload.reply_markup = new CalendarHelper(options).getCalendarMarkup(
+        options.startDate ?? new Date()
+      );
+    }
+  }
 
-useCalendar.callbackQuery(/calendar-telegram-ignore-[\d\w-]+/, async (ctx) => {
-  await ctx.answerCallbackQuery();
-});
+  middleware(): MiddlewareFn<T> {
+    const CalendarComposer = new Composer<T>((ctx, next) => {
+      ctx.api.config.use(async (prev, method, payload, signal) => {
+        const p: Record<string, unknown> = payload;
+        if (Array.isArray(p.results)) {
+          p.results.map((r) => this.prepare(r, ctx));
+        } else {
+          this.prepare(p, ctx);
+        }
+        return await prev(method, payload, signal);
+      });
+      return next();
+    });
+
+    CalendarComposer.callbackQuery(
+      /calendar-telegram-date-([\d-]+)/,
+      async (ctx, next) => {
+        if (!ctx.match) return;
+        const dateString = ctx.match[1];
+        ctx.calendarSelectedDate = new Date(dateString);
+        await ctx.answerCallbackQuery();
+        await ctx.editMessageReplyMarkup();
+        await next();
+      }
+    );
+
+    CalendarComposer.callbackQuery(
+      /calendar-telegram-(prev|next)-([\d-]+)/,
+      async (ctx) => {
+        if (!ctx.match) return;
+        const which = ctx.match[1];
+        const dateString = ctx.match[2];
+        const date = new Date(dateString);
+        date.setMonth(
+          which === "next" ? date.getMonth() + 1 : date.getMonth() - 1
+        );
+        await ctx.answerCallbackQuery();
+        await ctx.editMessageReplyMarkup({
+          reply_markup: new CalendarHelper(
+            this.extractOptions(ctx)
+          ).getCalendarMarkup(date),
+        });
+      }
+    );
+
+    CalendarComposer.callbackQuery(
+      /calendar-telegram-ignore-[\d\w-]+/,
+      async (ctx) => {
+        await ctx.answerCallbackQuery();
+      }
+    );
+    return CalendarComposer.middleware();
+  }
+}
 
 export type CalendarContext = {
   calendarSelectedDate?: Date;
 };
+
+type CustomContext = Context & CalendarContext;
